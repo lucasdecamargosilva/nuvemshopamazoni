4b2024b010166ec1c02240156733ecced9bce365
Lucas de Camargo Silva <cionilucas@gmail.com>
2026-04-30 12:05:45 -0300

    style: adjust stamp button to top:55px

--- widget-amazoni.js
+++ widget-amazoni.js
@@ -123,7 +123,7 @@
         }
         .q-btn-trigger-ia {
             position: absolute;
-            top: 80px; right: 15px;
+            top: 55px; right: 15px;
             z-index: 100;
             background: none; border: none; padding: 0;
             cursor: pointer;
